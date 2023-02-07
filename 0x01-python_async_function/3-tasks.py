7d2e5020041c2fceafa99518891e466b485d2696
HOGBONOUTO kentin <kentinhogbonouto1@gmail.com>
2023-02-07 01:29:36 +0100

    fix: made some change

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-""" Regular function that returns an asyncio task """
+"""Create a task"""
 import asyncio
-from typing import Awaitable
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[int]:
-    """ Regular function that returns an asyncio task """
-    return (asyncio.create_task(wait_random(max_delay)))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a task"""
+    return asyncio.create_task(wait_random(max_delay))
